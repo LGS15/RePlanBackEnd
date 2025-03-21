c7f50ec820bcf86e69ed53117875be8a12ae2b3a
Matei Copoeru <m.copoeru@student.fontys.nl>
2025-03-21 14:23:38 +0100

    Added first implementation for TeamRepository which is InMemoryTeamRepository.

---
+++ APIcred/src/main/java/com/replan/repos/InMemoryTeamRepository.java
@@ -0,0 +1,31 @@
+package com.replan.repos;
+
+import com.replan.repos.TeamRepository;
+import com.replan.domain.objects.Team;
+import org.springframework.stereotype.Repository;
+import java.util.*;
+
+@Repository
+public class InMemoryTeamRepository implements TeamRepository {
+    private final Map<String, Team> storage= new HashMap<>();
+
+    @Override
+    public Team save(Team team) {
+        if (team.getId() == null || team.getId().isEmpty())
+        {
+            team.setId(UUID.randomUUID().toString());
+        }
+        storage.put(team.getId(), team);
+        return team;
+    }
+
+    @Override
+    public Optional<Team> findById(String id){
+        return Optional.ofNullable(storage.get(id));
+    }
+
+    @Override
+    public List<Team> findAll() {
+        return new ArrayList<>(storage.values());
+    }
+}
--- APIcred/src/main/java/com/replan/repos/TeamRepository.java
+++ APIcred/src/main/java/com/replan/repos/TeamRepository.java
@@ -1,4 +1,12 @@
 package com.replan.repos;
 
-public class TeamRepository {
+import com.replan.domain.objects.Team;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TeamRepository {
+    Team save(Team team);
+    Optional<Team> findById(String id);
+    List<Team> findAll();
 }
