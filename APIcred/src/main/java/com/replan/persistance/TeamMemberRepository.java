2aa249e82997dd9311798ca3b84cade512f55347
Matei Copoeru <m.copoeru@student.fontys.nl>
2025-03-26 14:30:30 +0100

    No real change but testing for the Runner

--- APIcred/src/main/java/com/replan/persistance/TeamMemberRepository.java
+++ APIcred/src/main/java/com/replan/persistance/TeamMemberRepository.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface TeamMemberRepository {
+
     TeamMember save(TeamMember teamMember);
     Optional<TeamMember> findById(String id);
     List<TeamMember> findByTeamId(String teamId);
