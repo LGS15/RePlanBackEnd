de26eea122d58b57c81742af0cd24cb43493ac09
Matei Copoeru <m.copoeru@student.fontys.nl>
2025-03-27 23:02:08 +0100

    Added CORS rule on controller

--- APIcred/src/main/java/com/replan/controller/TeamController.java
+++ APIcred/src/main/java/com/replan/controller/TeamController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.*;
 
-
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/teams")
 public class TeamController {
