68304183aed3f5b979e67e58cc1a2c66c1307606
Matei Copoeru <m.copoeru@student.fontys.nl>
2025-04-17 23:27:31 +0200

    Adding SonarQube

--- APIcred/build.gradle.kts
+++ APIcred/build.gradle.kts
@@ -2,22 +2,16 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.4.3"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("org.sonarqube") version "6.0.1.5171"
 }
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
 
+
 java {
 	toolchain {
-		languageVersion = JavaLanguageVersion.of(17)
-	}
-}
-
-
-
-configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
+		languageVersion.set(JavaLanguageVersion.of(17))
 	}
 }
 
@@ -31,8 +25,18 @@ dependencies {
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("com.mysql:mysql-connector-j")
 	annotationProcessor("org.projectlombok:lombok")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+sonar {
+	properties {
+		property("sonar.projectKey", "RePlan-Backend")
+		property("sonar.projectName", "RePlan Backend")
+		property("sonar.host.url", "http://localhost:9000")
+		property("sonar.token", "sqp_3d2af99752301f76dd0cce3504772fdccc2197cb")
+	}
 }
 
 tasks.withType<Test> {
