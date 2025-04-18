97b89ebdf937225c4eec54c45750090a1694458f
Matei Copoeru <m.copoeru@student.fontys.nl>
2025-04-18 23:48:14 +0200

    Fixed the TeamControllerTest

--- APIcred/src/test/java/com/replan/team/TeamControllerTest.java
+++ APIcred/src/test/java/com/replan/team/TeamControllerTest.java
@@ -13,6 +13,7 @@ import com.replan.domain.responses.AddTeamMemberResponse;
 import com.replan.domain.responses.CreateTeamResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
@@ -23,6 +24,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(TeamController.class)
+@AutoConfigureMockMvc(addFilters = false)    // disable Spring Security filters
 @Import(TeamTestConfig.class)
 public class TeamControllerTest {
 
