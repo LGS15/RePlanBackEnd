81ef3fc1749212d228c73125405caf87bb8a731d
Matei Copoeru <m.copoeru@student.fontys.nl>
2025-04-18 23:59:34 +0200

    Moved config

--- APIcred/src/main/java/com/replan/config/WebConfig.java
+++ APIcred/src/main/java/com/replan/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.replan.controller;
+package com.replan.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
